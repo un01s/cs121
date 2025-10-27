77daf1a0a6f7074b25e066d60d51511923f55933
un01s <96022876+un01s@users.noreply.github.com>
2025-10-27 11:08:43 -0600

    update

--- cs61b/code/02-numbers/HelloNumbers.java
+++ cs61b/code/02-numbers/HelloNumbers.java
@@ -1,9 +1,21 @@
+/*
+  Java comments are non-executable things used to explain code
+  for readability. Code is mainly for other programmer.
+  
+  single-line comments start with //
+  multiple-line commments
+  
+  ex 1.1.2: print out 0 1 3 6 10 ... and end with 45
+  
+ */
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
+        int y = 0;
         while (x < 10) {
-            System.out.print(x + " ");
-            x = x + 1;
+            System.out.print(y + " ");
+            x = x + 1; //x: 0 1 2 3 4 5 6 7 8 9
+            y = x + y; //y: 0 1 3 6 10 15 21 28 36 45
         }
     }
 }
--- cs61b/code/README.md
+++ cs61b/code/README.md
@@ -9,10 +9,14 @@ $ which javac
 /usr/bin/javac
 $ javac --version
 javac 21.0.5
+$
 $ javac HelloWorld.java
 $ java HelloWorld
 Hello, World!
 ```
+
+Compiler: javac
+interpreter: java
 
 ## 02. Hello numbers
 
@@ -22,3 +26,8 @@ $ java HelloNumbers
 0 1 2 3 4 5 6 7 8 9
 ```
 
+variables and while-loop
+
+* x must be declared before it is used, and it must be given a type, that is ```int```
+* Java is a statically typed language. It means that all variables, parameters, and methods must have a declared type.
+ 
